4e40033fa1c75ad809984905ac6c665485dc10f6
smaarmstrong <108431423+smaarmstrong@users.noreply.github.com>
2025-03-23 20:18:02 +0000

    postcss fix (#2)

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react(), wasm(), topLevelAwait()],
   server: {
     fs: {
-      // Allow serving files from one level up to the project root
-      allow: ["plutus.json"],
+      allow: [
+        path.resolve(__dirname, '.'),
+        "plutus.json",
+        path.resolve(__dirname, 'src'),
+        path.resolve(__dirname, 'node_modules')
+      ],
     },
   },
 });

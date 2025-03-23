eb2bba789ee02c03a27ad47de4c5abf712da64c7
smaarmstrong <108431423+smaarmstrong@users.noreply.github.com>
2025-03-23 20:31:50 +0000

    homepage setup -- caveat in PR (#3)
    * homepage setup -- caveat in PR
    
    * other files I forgot to commit

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,13 @@ export default defineConfig({
         path.resolve(__dirname, '.'),
         "plutus.json",
         path.resolve(__dirname, 'src'),
-        path.resolve(__dirname, 'node_modules')
+        path.resolve(__dirname, 'node_modules'),
       ],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
